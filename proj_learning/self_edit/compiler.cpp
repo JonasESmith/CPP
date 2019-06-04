404433f65c79586f66895a28f2aae279039fcebd
Jonas Smith <pictofoster@gmail.com>
2019-06-04 10:35:14 -0500

    Messing with more ideas

--- proj_learning/self_edit/compiler.cpp
+++ proj_learning/self_edit/compiler.cpp
@@ -6,6 +6,7 @@
 // allows us to skip using std::
 using namespace std;
 
+std::string removeSpaces(string inputValue);
 
 vector<string> codeVector;
 
@@ -25,5 +26,40 @@ int main() {
         }
     }
 
+    string lineBefore = "{";
+
+    for(int i = 0; i < codeVector.size(); i++)
+    {
+        cout << codeVector[i] << "\n";
+        if( removeSpaces( codeVector[i]) == removeSpaces( lineBefore))
+        {
+            cout << "    // They are equal!\n";
+        }
+    }
+
+    int i;
+    printf ("Checking if processor is available...");
+    if (system(NULL)) puts ("Ok");
+        else exit (EXIT_FAILURE);
+    printf ("Executing command DIR...\n");
+    i=system ("dir");
+    printf ("The value returned was: %d.\n",i);
+     printf ("Executing command ./main...\n");
+    i=system ("psexec -i -s main.exe");
+    printf ("The value returned was: %d.\n",i);
+    
+    cout << i << "\n";
+
     return 0;
+}
+
+std::string removeSpaces(string inputValue)
+{
+    string valueWithoutSpaces;
+
+    for(int i = 0; i < inputValue.size(); i++)
+        if(inputValue[i] != ' ')
+            valueWithoutSpaces += inputValue[i];
+
+    return valueWithoutSpaces;
 }
